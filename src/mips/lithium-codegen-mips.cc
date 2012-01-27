9339d6635ec89e11c28d916f0322d148851593f1
Akos Palfi <palfia@homejinni.com>
2012-01-27 02:33:06 +0100

    Revert "[MIPS crankshaft merge] Fix asserts and GC unsafeness in stub generation"
    This reverts commit db85811aa9556b149264b7fc990fb5cdb8de74e2.

--- src/mips/lithium-codegen-mips.cc
+++ src/mips/lithium-codegen-mips.cc
@@ -82,11 +82,9 @@ bool LCodeGen::GenerateCode() {
   status_ = GENERATING;
   CpuFeatures::Scope scope(FPU);
 
-  CodeStub::GenerateFPStubs();
-
   // Open a frame scope to indicate that there is a frame on the stack.  The
   // NONE indicates that the scope shouldn't actually generate code to set up
-  // the frame (that is done in GeneratePrologue).
+  // the frame (that is done in GeneatePrologue).
   FrameScope frame_scope(masm_, StackFrame::NONE);
 
   return GeneratePrologue() &&
